d1c6982bb34d1598c7dc0bfd5c2ee82fb9331d4b
alberthamoui <albertdh@al.insper.edu.br>
2026-08-31 17:31:31 -0300

    Refactor code structure for improved readability and maintainability

--- baseDeDados.py
+++ baseDeDados.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-with open('wordle.txt', 'r') as arquivo:
+with open('data/wordle.txt', 'r') as arquivo:
     conteudo = arquivo.read()
 
 palavras = conteudo.lower()
@@ -12,7 +12,7 @@ for palavra in palavras:
     if palavra not in lista_wordle:
         lista_wordle.append(palavra)
 
-df = pd.read_csv("palavras_termo.csv")
+df = pd.read_csv("data/palavras_termo.csv")
 
 if "palavras" in df.columns:
 	lista_termo = [p.lower() for p in df["palavras"].tolist()]
@@ -21,5 +21,5 @@ elif "Palavras" in df.columns:
 else:
 	lista_termo = [p.lower() for p in df.iloc[:, 0].tolist()]
 
-# print(f'len w: {len(lista_wordle)}' )
-# print(f'len t: {len(lista_termo)}' )
+# print(f'len w: {len(lista_wordle)}')
+# print(f'len t: {len(lista_termo)}')
